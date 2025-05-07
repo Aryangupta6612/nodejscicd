623e9751d0d169fb1f410921ef1cc8dabff427af
Aryangupta6612 <aryan.gupta6612@gmail.com>
2025-05-07 23:42:21 +0530

    latest commit

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ const server = http.createServer((req, res) => {
   res.end('Hello World\n');
 });
 
-server.listen(3000, () => {
+server.listen(4000, () => {
   console.log('Server is running at http://localhost:3000');
 });
--- dockerfile
+++ dockerfile
@@ -2,5 +2,5 @@ FROM node:14
 WORKDIR /app
 COPY . .
 RUN npm install
-EXPOSE 3000
+EXPOSE 4000
 CMD ["npm", "start"]
